6fac10e79c6b3fc8253f11c5ce16f49647c484cb
rohimansupriyadi82-cmd <rohimansupriyadi82@admin.sd.belajar.id>
2026-06-02 10:00:26 +0700

    Set base path in vite.config for GitHub Pages
    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Tambahan baris base di bawah ini agar file aset Anda terbaca dengan benar di GitHub Pages
+  base: "/Cetak_Label_Sampul_Raport/",
   plugins: [react()],
   resolve: {
     alias: {
